7e17ab3c815cd3f37ccb5e321dc97bb74ce77a35
spacerest <sadiemparker@gmail.com>
2019-08-12 17:48:08 +0930

    making resources a module

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 setup(
     name = 'moonmask',
     packages = find_packages(),
-    version = '2.1',
+    version = '2.2',
     license='MIT',
     description = 'Gets moon visualizations courtesy of Ernie Wright and uses them for artistic collages',
     author = 'Sadie Parker',
     author_email = 'sadiemparker@gmail.com',
     url = 'https://github.com/spacerest/moonmask',
-    download_url = 'https://github.com/spacerest/moonmask/archive/v_21.tar.gz',
+    download_url = 'https://github.com/spacerest/moonmask/archive/v_22.tar.gz',
     keywords = ['MOON', 'ART'],
     install_requires=[
         'atomicwrites==1.3.0',
