84287e041a0db50bcfa1779cf0aa69332ed6ee78
spacerest <sadiemparker@gmail.com>
2019-09-05 16:59:46 +0930

    new release

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 from setuptools import setup, find_packages
 
 # long_description
-readme_path = directory.joinpath("README.md")
+readme_path = "./README.md"
 with readme_path.open() as read_file:
     long_description = read_file.read()
 
 setup(
     name = 'dialamoon',
     packages = find_packages(),
-    version = '1.2',
+    version = '1.3',
     license='MIT',
     description = 'Gets moon visualizations courtesy of NASA/Ernie Wright',
     long_description_content_type="text/markdown",
@@ -17,7 +17,7 @@ setup(
     author = 'Sadie Parker',
     author_email = 'sadiemparker@gmail.com',
     url = 'https://github.com/spacerest/dialamoon',
-    download_url = 'https://github.com/spacerest/dialamoon/archive/v_1_2.tar.gz',
+    download_url = 'https://github.com/spacerest/dialamoon/archive/v_1_3.tar.gz',
     keywords = ['MOON', 'ART'],
     install_requires=[
         'numpy~=1.16',
